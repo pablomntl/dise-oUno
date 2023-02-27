84b190edda8e49b2438f9e18355cfda567c36156
pablo mereles <pablomereles62@gmail.com>
2023-02-27 17:09:51 -0300

    modifiqué el menu

---
+++ main.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector("#menu");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    menu.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    menu.classList.remove("visible");
+})
